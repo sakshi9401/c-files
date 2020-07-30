bebfd4c2defd5fbb13e4bdb93f0978f91a3dd71e
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-30 20:04:42 +0530

    Add files via upload

--- sum of two no inp frm user.cpp
+++ sum of two no inp frm user.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 int main()
-{
 using namespace std;
-int n1,n2,sum
-sum=n1+n1;
-cout<<"enter two no";
-cin>>
-cout<<"the sum of two no is"<<sum;
+{
+int a,b,sum;
+cout<<"enter two no.";
+cin>>a>>b;
+cout<<"the sum of "<<a<<"&"<<b<<"is"<<sum;
+return0;
 }
