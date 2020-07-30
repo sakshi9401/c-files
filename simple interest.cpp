ecd1fc4764e185f76e4d266ee94958bbcd990c25
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-30 20:16:38 +0530

    Add files via upload

---
+++ simple interest.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int p,r,t,m;
+float SI;
+cout<<"enter the values of p,r and t";
+cin>>p>>r>>t;
+m=p*r*t;
+SI=m/100;
+cout<<"the simple interest is"<<SI;
+}
