29922ac84d367445bac58d278457b5998fd5bb21
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-30 20:10:30 +0530

    Add files via upload

--- division of 2 no.cpp
+++ division of 2 no.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
-using namaespace std;
+using namespace std;
 int main()
 {
 int a,b,divi;
-cout<<"enter two no.";
+cout<<"enter two no";
 cin>>a>>b;
 divi=a/b;
 cout<<"the division of"<<a<<"&"<<b<<"is"<<divi;
 }
-s
